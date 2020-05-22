436386d53eab86be31996d8bf1114b8cb5913ea3
vicky-wjz <52119912+vicky-wjz@users.noreply.github.com>
2020-05-22 16:03:14 +1000

    initial project

---
+++ src/homepage.component.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './homepage.scss';
+
+const HomePage = () => (
+	<div className='homepage'>
+		<div className='directory-menu'>
+			<div className='menu-item'>
+				<div className='content'>
+					<h1 className='title'>Cat Food</h1>
+					<span className='subtitle'>SHOP NOW</span>
+				</div>
+			</div>
+			<div className='menu-item'>
+				<div className='content'>
+					<h1 className='title'>Litter</h1>
+					<span className='subtitle'>SHOP NOW</span>
+				</div>
+			</div>
+			<div className='menu-item'>
+				<div className='content'>
+					<h1 className='title'>Flea and Worming</h1>
+					<span className='subtitle'>SHOP NOW</span>
+				</div>
+			</div>
+			<div className='menu-item'>
+				<div className='content'>
+					<h1 className='title'>Treats</h1>
+					<span className='subtitle'>SHOP NOW</span>
+				</div>
+			</div>
+			<div className='menu-item'>
+				<div className='content'>
+					<h1 className='title'>Toys</h1>
+					<span className='subtitle'>SHOP NOW</span>
+				</div>
+			</div>
+		</div>
+	</div>		
+
+	)
+
+export default HomePage
